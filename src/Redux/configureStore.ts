97f5f3e914e536d93c46f602fc0fc401aee84b19
Demchuk Tatyana <69cobak@mail.ru>
2021-03-21 09:50:22 +0300

    feat: init app, add base routing

---
+++ src/Redux/configureStore.ts
@@ -0,0 +1,19 @@
+import { createBrowserHistory } from 'history'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { routerMiddleware } from 'connected-react-router'
+import createRootReducer from './reducers'
+
+export const history = createBrowserHistory()
+
+export default (preloadedState:any) => {
+    const store = createStore(
+        createRootReducer(history), // root reducer with router state
+        preloadedState,
+        compose(
+            applyMiddleware(
+                routerMiddleware(history), // for dispatching history actions
+            ),
+        ),
+    )
+    return store
+}
