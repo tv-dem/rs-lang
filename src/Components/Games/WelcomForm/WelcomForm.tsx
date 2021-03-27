ae6429f33278126ae044320f85c9bdf2ae4e46d6
kli2m <ivanklimm87@gmail.com>
2021-03-27 16:21:42 +0300

    feat: fixed WelcomeForm

--- src/Components/Games/WelcomForm/WelcomForm.tsx
+++ src/Components/Games/WelcomForm/WelcomForm.tsx
@@ -1,76 +1,45 @@
 import React, { useState, useRef } from "react";
-import {
-  Button,
-  Typography,
-  Form,
-  Select,
-  Input,
-} from "antd";
+import { Button, Typography, Form, Select, Input } from "antd";
 import "./WelcomForm.scss";
 
 const WelcomForm = () => {
-
-
   const { Text, Title } = Typography;
 
-
-
   return (
-   
-          <div className="letterSolver__settings">
-            <Title level={3} className="letterSolver__start_game-title">
-              <Text strong>TITLE</Text>
-            </Title>
-            <Form >
-              <Form.Item
-                name={["user", "name"]}
-                label="Name"
-              >
-                <Input
-                  placeholder="Name"
-                 
-                />
-              </Form.Item>
-       
-              <Form.Item
-                name={["user", "wordsLevel"]}
-                label="WORD DIFFICULT"
-                initialValue="0"
-              >
-                <Select
-                  placeholder="LEVEL DIFFICULT"
-                >
-                  <Select.Option value="0">
-                  FIRST
-                  </Select.Option>
-                  <Select.Option value="1">
-                   SECOND
-                  </Select.Option>
-                  <Select.Option value="2">
-                  THIRD
-                  </Select.Option>
-                  <Select.Option value="3">
-                   FOURTH
-                  </Select.Option>
-                  <Select.Option value="4">
-                    FIFTH
-                  </Select.Option>
-                  <Select.Option value="5">
-                  SIXTH
-                  </Select.Option>
-                </Select>
-              </Form.Item>           
+    <div className="welcomeForm">
+      <Title level={3} className="welcomeForm-title">
+        <Text strong>TITLE</Text>
+      </Title>
 
-              <Button
-                className="letterSolver__start_game-btn"
-                autoFocus
-                htmlType="submit"
-              >
-               START
-              </Button>
-            </Form>
-          </div>
-      
+      <Form className="welcomeForm__form">
+        <Form.Item
+          className="welcomeForm__form-item"
+          name={["user", "wordsLevel"]}
+          label="WORD DIFFICULT"
+          initialValue="0"
+        >
+          <Select
+            className="welcomeForm__form-select"
+            placeholder="LEVEL DIFFICULT"
+          >
+            <Select.Option value="0">FIRST</Select.Option>
+            <Select.Option value="1">SECOND</Select.Option>
+            <Select.Option value="2">THIRD</Select.Option>
+            <Select.Option value="3">FOURTH</Select.Option>
+            <Select.Option value="4">FIFTH</Select.Option>
+            <Select.Option value="5">SIXTH</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Button
+          className="welcomeForm__form-btnStart"
+          autoFocus
+          htmlType="submit"
+        >
+          START
+        </Button>
+      </Form>
+    </div>
   );
 };
 export default WelcomForm;
