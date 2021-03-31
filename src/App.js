bd42da79dceddb3cd081ae21d3a365602d162e2e
funfordima <d.g.litvinov@gmail.com>
2021-03-31 23:03:26 +0300

    feat: add prettier, @types, set deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {Route, Switch, useLocation} from "react-router";
+import React, { useEffect } from 'react';
+import { Route, Switch, useLocation } from "react-router";
 import NavPagesContainer from "./Components/NavPages/NavPagesContainer";
 
 import {
@@ -9,7 +9,7 @@ import {
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import FooterContainer from "./Components/Footer/FooterContainer";
 import WelcomFormContainer from './Components/Games/WelcomForm/WelcomFormContainer'
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import MainPageContainer from "./Pages/MainPage/MainPageContainer";
 import TextBookContainer from "./Pages/TextBook/TextBookContainer";
 import GamesContainer from "./Pages/Games/GamesContainer";
@@ -18,37 +18,40 @@ import DictionaryContainer from "./Pages/Dictionary/DictionaryContainer";
 import LetterSolverContainer from './Components/Games/LetterSolver/LetterSolverContainer'
 
 function App() {
-  useEffect(()=>{
-    window.scrollTo(0,0)
-  })
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  });
+
   let location = useLocation();
-  return ( <>
-    <HeaderContainer/>
-    <NavPagesContainer/>
-    <div className='content'>
-      <TransitionGroup className="transition-group">
-        <CSSTransition
-          key={location.key}
-          classNames="fade"
-          timeout={500}
-        >
-          <Switch>
-            <Route path='/games/LetterSolver/:level' component={LetterSolverContainer} />
-            <Route path='/games/Sprint/:level' component={LetterSolverContainer} />
-            <Route path='/games/Savanna/:level' component={LetterSolverContainer} />
-            <Route path='/games/AudioCall/:level' component={LetterSolverContainer} />
-            <Route path='/games/:game' component={WelcomFormContainer} />
-            <Route path='/games' component={GamesContainer} />
-            <Route path='/statistic' component={StatisticContainer} />
-            <Route path='/dictionary/:section/:page' component={DictionaryContainer} />
-            <Route path='/textbook/:level/:page' component={TextBookContainer} />
-            <Route path='/home' component={MainPageContainer} />
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
-      {/*<Redirect from='/' to='/'/>*/}
-    </div>
-    <FooterContainer/>
+
+  return (
+    <>
+      <HeaderContainer />
+      <NavPagesContainer />
+      <div className='content'>
+        <TransitionGroup className="transition-group">
+          <CSSTransition
+            key={location.key}
+            classNames="fade"
+            timeout={500}
+          >
+            <Switch>
+              <Route path='/games/LetterSolver/:level' component={LetterSolverContainer} />
+              <Route path='/games/Sprint/:level' component={LetterSolverContainer} />
+              <Route path='/games/Savanna/:level' component={LetterSolverContainer} />
+              <Route path='/games/AudioCall/:level' component={LetterSolverContainer} />
+              <Route path='/games/:game' component={WelcomFormContainer} />
+              <Route path='/games' component={GamesContainer} />
+              <Route path='/statistic' component={StatisticContainer} />
+              <Route path='/dictionary/:section/:page' component={DictionaryContainer} />
+              <Route path='/textbook/:level/:page' component={TextBookContainer} />
+              <Route path='/home' component={MainPageContainer} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+        {/*<Redirect to='/'/>*/}
+      </div>
+      <FooterContainer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss'
-import {Provider} from "react-redux";
-import {Switch, Route} from "react-router";
-import {ConnectedRouter} from 'connected-react-router'
-import configureStore, { history } from './Redux/configureStore'
-import {BrowserRouter} from "react-router-dom";
+import { Provider } from "react-redux";
+import { Route } from "react-router";
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from './Redux/configureStore';
 
 const store = configureStore(); // null вместо initState
 
 ReactDOM.render(
-  <React.StrictMode>
+  (<React.StrictMode>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-          <Route path='/' component={App} />
+        <Route path='/' component={App} />
       </ConnectedRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>)
   ,
   document.getElementById('root')
 );
