0c60bedd2042e9c91d7f9f714540e835d3bc0c50
funfordima <d.g.litvinov@gmail.com>
2021-04-10 16:55:09 +0300

    feat: add fetch methods: get, set user statistics

--- src/API/API.ts
+++ src/API/API.ts
@@ -125,63 +125,99 @@ class API {
       })
     })
   }
-    updateUserWord(wordId: string, userId: string, difficulty: string, optional: Object, token: string) {
-        return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/words/${wordId}`, {
-            method: 'PUT',
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                difficulty,
-                optional
-            })
-        })
-            .then(res => res.json())
-    }
+  updateUserWord(wordId: string, userId: string, difficulty: string, optional: Object, token: string) {
+    return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/words/${wordId}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        difficulty,
+        optional
+      })
+    })
+      .then(res => res.json())
+  }
 
-    getAggregatedWords(userId: string, group: number, page: number, token: string) {
-        return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/aggregatedWords?group=${group}&page=${page}&wordsPerPage=20`, {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-                if (res.status === 401) {
-                    return null;
-                }
-                return null
-            })
-    }
+  getAggregatedWords(userId: string, group: number, page: number, token: string) {
+    return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/aggregatedWords?group=${group}&page=${page}&wordsPerPage=20`, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        if (res.status === 401) {
+          return null;
+        }
+        return null
+      })
+  }
 
-    getAggregatedWordsByParams(userId: string, params:any, group:number, page:number, token: string){
-        const filter = JSON.stringify(params);
-        return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/aggregatedWords?filter=${filter}&group=${group}&page=${page}&wordsPerPage=20`, {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-                if (res.status === 401) {
-                    return null;
-                }
-                return null
-            })
-    }
+  getAggregatedWordsByParams(userId: string, params: any, group: number, page: number, token: string) {
+    const filter = JSON.stringify(params);
+    return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/aggregatedWords?filter=${filter}&group=${group}&page=${page}&wordsPerPage=20`, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        if (res.status === 401) {
+          return null;
+        }
+        return null
+      })
+  }
+
+  getUserStatistics(userId: string, token: string) {
+    return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/statistics`, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error(`${res.status}`);
+        }
+
+        return res.json();
+      });
+  }
+
+  setUserStatistics(userId: string, token: string, body: any) {
+    return fetch(`https://api-rs-lang.herokuapp.com/users/${userId}/statistics`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error(`${res.status}`);
+        }
+
+        return res.json();
+      });
+  }
 }
-
 
 export default new API();
