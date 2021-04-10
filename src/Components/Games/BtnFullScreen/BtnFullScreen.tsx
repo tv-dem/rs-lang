5141278b878151e95b3983e9a254cf5fe969d172
sovanmarat <maratwp@mail.ru>
2021-04-10 18:07:31 +0300

    fix: fixed btnFullScreen

--- src/Components/Games/BtnFullScreen/BtnFullScreen.tsx
+++ src/Components/Games/BtnFullScreen/BtnFullScreen.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Button } from "antd";
 import fullScreen from "../../../utils/fullScreen";
 
 const BtnFullScreen: React.FC = () => {
 
   return (
-    <Button className="fullScreenBtn" type="primary" aria-hidden="true" onClick={() => fullScreen()}>full</Button>
+    <div className="fullScreenBtn btn" onClick={() => fullScreen()}></div>
   )
 }
 
