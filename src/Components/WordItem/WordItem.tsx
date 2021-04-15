5436c32c21014bfed78a3fc43adfb8a4cb3a1cde
funfordima <d.g.litvinov@gmail.com>
2021-04-15 23:47:31 +0300

    fix: add prop key to wordItem.tsx

--- src/Components/WordItem/WordItem.tsx
+++ src/Components/WordItem/WordItem.tsx
@@ -1,51 +1,53 @@
-import React, {useState} from 'react';
-import {Popover} from 'antd';
+import React, { useState } from 'react';
+import { Popover } from 'antd';
 import './WordItem.scss';
-import {CustomerServiceOutlined} from "@ant-design/icons";
-import {connect} from "react-redux";
+import { CustomerServiceOutlined } from "@ant-design/icons";
+import { connect } from "react-redux";
 
-const WordItem = ({word,modificator,option,translate, audio,audioMeaning,audioExample,textMeaning, textExample, image, transcription, textExampleTranslate, textMeaningTranslate, wordTranslate, options}: any) => {
-    const [audios] = useState([
-        new Audio('https://api-rs-lang.herokuapp.com/'+audio),
-        new Audio('https://api-rs-lang.herokuapp.com/'+audioMeaning),
-        new Audio('https://api-rs-lang.herokuapp.com/'+audioExample),
-    ])
-  return <div className={`word-item ${modificator}`}>
-        <div className="word-item__info">
-            <img
-                src={'https://api-rs-lang.herokuapp.com/'+image}
-                alt='...'
-            />
-            <CustomerServiceOutlined onClick={()=>{
-                audios[0].play();
-                audios[0].addEventListener('ended', ()=> audios[1].play());
-                audios[1].addEventListener('ended', ()=>audios[2].play());
-            }}/>
-            <Popover
-                content={() => <div>
-                  <p dangerouslySetInnerHTML={{__html: textMeaning}}/>
-                    {
-                      // @ts-ignore
-                      translate && <p>{textMeaningTranslate}</p>
-                    }
-                    <br/>
-                    <p dangerouslySetInnerHTML = {{__html:textExample}}/>
-                    {translate && <p>{textExampleTranslate}</p>}
-                </div>}
-                title={`${word} - ${transcription}`}>
-                <div className="words">
-                    <span>{word} - {transcription}</span>
-                    {translate && <span>{wordTranslate}</span>}
-                </div>
-            </Popover>
-        </div>
-        {option && options && <div className="word-item__setting">
-            {options.map(({title, onClick}: any) => <span onClick={onClick}>{title}</span>)}
-        </div>}
+const WordItem = ({ word, modificator, option, translate, audio, audioMeaning, audioExample, textMeaning, textExample, image, transcription, textExampleTranslate, textMeaningTranslate, wordTranslate, options, key }: any) => {
+  const [audios] = useState([
+    new Audio('https://api-rs-lang.herokuapp.com/' + audio),
+    new Audio('https://api-rs-lang.herokuapp.com/' + audioMeaning),
+    new Audio('https://api-rs-lang.herokuapp.com/' + audioExample),
+  ])
+  return (
+    <div className={`word-item ${modificator}`} key={key}>
+      <div className="word-item__info">
+        <img
+          src={'https://api-rs-lang.herokuapp.com/' + image}
+          alt='...'
+        />
+        <CustomerServiceOutlined onClick={() => {
+          audios[0].play();
+          audios[0].addEventListener('ended', () => audios[1].play());
+          audios[1].addEventListener('ended', () => audios[2].play());
+        }} />
+        <Popover
+          content={() => <div>
+            <p dangerouslySetInnerHTML={{ __html: textMeaning }} />
+            {
+              // @ts-ignore
+              translate && <p>{textMeaningTranslate}</p>
+            }
+            <br />
+            <p dangerouslySetInnerHTML={{ __html: textExample }} />
+            {translate && <p>{textExampleTranslate}</p>}
+          </div>}
+          title={`${word} - ${transcription}`}>
+          <div className="words">
+            <span>{word} - {transcription}</span>
+            {translate && <span>{wordTranslate}</span>}
+          </div>
+        </Popover>
+      </div>
+      {option && options && <div className="word-item__setting">
+        {options.map(({ title, onClick }: any) => <span key={title} onClick={onClick}>{title}</span>)}
+      </div>}
     </div>
+  );
 }
 
-const mapStateToProps = ({auth}:any) => ({
+const mapStateToProps = ({ auth }: any) => ({
   option: auth.userSettings.options,
   translate: auth.userSettings.translate,
 })
--- src/Pages/TextBook/TextBookContent/TextBookContent.tsx
+++ src/Pages/TextBook/TextBookContent/TextBookContent.tsx
@@ -2,18 +2,18 @@ import React, { FC } from 'react';
 import './TextBookContent.scss';
 import WordItem from "../../../Components/WordItem/WordItem";
 
-const TextBookContent: FC = ({words, options, createUserWord,updateUserWord, userId, token,currLevel,currPage}: any) => {
+const TextBookContent: FC = ({ words, options, createUserWord, updateUserWord, userId, token, currLevel, currPage }: any) => {
 
-    const optionOnClick = (userWord:any, id: string, section:string) => {
-        if(userWord){
-            updateUserWord(id, userId, section, {}, token,currLevel-1,currPage-1)
-        } else{
-            createUserWord(id, userId, section, {}, token,currLevel-1,currPage-1);
+    const optionOnClick = (userWord: any, id: string, section: string) => {
+        if (userWord) {
+            updateUserWord(id, userId, section, {}, token, currLevel - 1, currPage - 1)
+        } else {
+            createUserWord(id, userId, section, {}, token, currLevel - 1, currPage - 1);
         }
     }
 
     return <div className={`text-book__content h${currLevel}`}>
-        {words.map(({id, _id, userWord, word, audio, audioMeaning, audioExample, textMeaning, image, textExample, transcription, textExampleTranslate, textMeaningTranslate, wordTranslate}: any, i: number) => {
+        {words.map(({ id, _id, userWord, word, audio, audioMeaning, audioExample, textMeaning, image, textExample, transcription, textExampleTranslate, textMeaningTranslate, wordTranslate }: any, i: number) => {
             return <WordItem
                 modificator={userWord ? userWord.difficulty : ''}
                 audioExample={audioExample}
@@ -28,19 +28,20 @@ const TextBookContent: FC = ({words, options, createUserWord,updateUserWord, use
                 textExampleTranslate={textExampleTranslate}
                 textMeaningTranslate={textMeaningTranslate}
                 wordTranslate={wordTranslate}
-                options={!_id ? null : options.map(({title, section}: any) => {
-                    if(userWord){
-                        if(userWord.difficulty === section){
+                options={!_id ? null : options.map(({ title, section }: any) => {
+                    if (userWord) {
+                        if (userWord.difficulty === section) {
                             return {};
                         }
                     }
                     return {
-                    title,
-                    onClick: () => optionOnClick(userWord,_id, section),
-                }})}
+                        title,
+                        onClick: () => optionOnClick(userWord, _id, section),
+                    }
+                })}
             />
         })}
-  </div>
+    </div>
 };
 
 export default TextBookContent;
