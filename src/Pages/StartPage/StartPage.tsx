340576aced3957334502ceabef4cf88a8369b9b5
sovanmarat <maratwp@mail.ru>
2021-04-15 21:36:04 +0300

    feat: added Start page

--- src/Pages/StartPage/StartPage.tsx
+++ src/Pages/StartPage/StartPage.tsx
@@ -6,15 +6,55 @@ const StartPage: React.FC = () => {
 
 
     return (
-<div className="Wrapper__StartPage">
-<div className="StartPage__content">
-<div className="StartPage__content_title">
-Изучи английский язык не выходя из дома!
+        <div className="Wrapper__StartPage">
+            <div className="StartPage__content">
+                <div className="StartPage__content_Box">
+                    <div className="StartPage__content_words">
+                        Ты расширишь свой словарный запас на 4000 слов, используя современную методику обучения.
+    </div>
+                    <div className="StartPage__content_title">
+                        Изучи английский язык не выходя из дома!
     </div>
 
+                </div>
+                <div className="StartPage__content_Box">
+
+                    <div className="StartPage__content_words_games">
+                        Повторение и изучение слов в игровой форме.
+    </div>
+                    <div className="StartPage__content_words_free">
+                        Бесплатный доступ. Наша миссия — сделать обучения языкам доступным для каждого.
+    </div>
+                </div>
+
+                <div className="Team_box_wrapper">
+                    <div className="Team_box_title">
+                        Над проектом работали:
+    </div>
+                    <div className="Team_box">
+
+                        <div className="Team_box_author">Климм Иван
+<div className="Team_box_author_city">
+                                Front-end Developer
+                                Витебск, Беларусь
 </div>
-</div>
-        )
-    }
-    
-    export default StartPage;
+<div className="Team_box_author_photo_Box">
+
+                            <div className="Team_box_author_photo">
+                            </div>
+                            </div>
+
+                        </div>
+                        <div className="Team_box_author">Sovan Marat</div>
+                        <div className="Team_box_author">Litvinov Dmitrij</div>
+                        <div className="Team_box_author">Tatyana</div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default StartPage;
